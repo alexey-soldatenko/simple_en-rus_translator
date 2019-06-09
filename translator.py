a7c9429f6035fd9f66b7fa040f723ca9bc332036
alexey-soldatenko <alexso1datenko86@gmail.com>
2019-06-09 12:22:55 +0300

    Refactore code

--- translator.py
+++ translator.py
@@ -11,75 +11,137 @@ from custom_std import (
     UNKNOWN)
 
 
-def get_translation_from_db(word, db_cursor):
-    db_cursor.execute("SELECT rus FROM dict where en='{}'".format(word))
-    r = db_cursor.fetchone()
-    return r[0] if r is not None else None
+class ExitCommand(Exception):
+    pass
 
 
-def print_help():
-    s = """
----------------------------------------------
-|    This is very simple en-rus translator. |
-|                                           |
-|    To execute command put '!' before.     |
-|    For example: !quit, !help              |
----------------------------------------------
-    """
-    print(s)
+class History:
+    def __init__(self):
+        self._previous = []
+        self._next = []
+
+    def add(self, word):
+        self._previous += self._next
+        self._previous.append(word)
+        self._next = []
+
+    def has_next(self):
+        return bool(self._next)
+
+    def has_previous(self):
+        return bool(self._previous)
+
+    def get_previous(self, current_word):
+        self._next.append(current_word)
+        return self._previous.pop()
+
+    def get_next(self, current_word):
+        self._previous.append(current_word)
+        return self._next.pop()
 
 
-def main():
-    print_help()
-    is_need_prompt = True
-    word_on_screen = ""
-    CACHE_PREVIOUS = []
-    CACHE_NEXT = []
-    while True:
-        if is_need_prompt:
+class Command:
+    def __init__(self):
+        self._history = History()
+        self.current_search_word = ""
+        self.need_prompt = True
+
+    def execute(self, command: str, **kwargs):
+        method_name = "_run_{}_command".format(command)
+        try:
+            run_method = getattr(self, method_name)
+        except AttributeError:
+            raise NotImplementedError("Not implemented method for '{}' command".format(command))
+        if callable(run_method):
+            run_method(**kwargs)
+
+    def _get_translation_from_db(self, word, db_cursor):
+        db_cursor.execute("SELECT rus FROM dict where en='{}'".format(word))
+        r = db_cursor.fetchone()
+        return r[0] if r is not None else None
+
+
+    def _run_help_command(self, **kwargs):
+        s = ("\n---------------------------------------------\n"
+             "|    This is very simple en-rus translator. |\n"
+             "|                                           |\n"
+             "|    To execute command put '!' before.     |\n"
+             "|    For example: !quit, !help              |\n"
+             "---------------------------------------------\n")
+        print(s)
+        self.current_search_word = ""
+        self.need_prompt = True
+
+
+    def _run_translate_command(self, search_word):
+        with sqlite3.connect('en-rus.db') as conn:
+            c = conn.cursor()
+
+            translation = self._get_translation_from_db(search_word, c)
+            if translation is not None:
+                colored_translation = "\x1b[96m{}\x1b[m".format(translation)
+                print("\n", colored_translation)
+            else:
+                print("\nTranslation not found!")
+            self.current_search_word = ""
+            self._history.add(search_word)
+        self.need_prompt = True
+
+
+    def _run_next_command(self, search_word):
+        if self._history.has_next():
+            next_word = self._history.get_next(search_word)
+            clear_stdout_previous_characters(len(search_word))
+            self.current_search_word = next_word
+            print(next_word, end="", flush=True)
+        self.need_prompt = False
+
+
+    def _run_previous_command(self, search_word):
+        if self._history.has_previous():
+            previous = self._history.get_previous(search_word)
+            clear_stdout_previous_characters(len(search_word))
+            self.current_search_word = previous
+            print(previous, end="", flush=True)
+        self.need_prompt = False
+
+
+    def _run_quit_command(self, **kwargs):
+        raise ExitCommand()
+
+
+    def _run_unknown_command(self, **kwargs):
+        print("\nNot valid command!")
+        self.need_prompt = True
+
+
+class Translator:
+    def __init__(self):
+        self._command = Command()
+
+    def execute_command(self, command: str, **kwargs):
+        self._command.execute(command, **kwargs)
+
+    def set_prompt_if_need(self):
+        if self._command.need_prompt:
             print(">>>", end=" ", flush=True)
-        search, cmd = get_search_word_with_command(word_on_screen)
-        if cmd == QUIT:
-            print("\nBuy!")
-            break
-        elif cmd == HELP:
-            print_help()
-            word_on_screen = ""
-            is_need_prompt = True
-        elif cmd == PREVIOUS:
-            if CACHE_PREVIOUS:
-                CACHE_NEXT.append(search)
-                previous = CACHE_PREVIOUS.pop()
-                clear_stdout_previous_characters(len(search))
-                word_on_screen = previous
-                print(previous, end="", flush=True)
-            is_need_prompt = False
-        elif cmd == NEXT:
-            if CACHE_NEXT:
-                CACHE_PREVIOUS.append(search)
-                next_word = CACHE_NEXT.pop()
-                clear_stdout_previous_characters(len(search))
-                word_on_screen = next_word
-                print(next_word, end="", flush=True)
-            is_need_prompt = False
-        elif cmd == TRANSLATE:
-            with sqlite3.connect('en-rus.db') as conn:
-                c = conn.cursor()
 
-                translation = get_translation_from_db(search, c)
-                if translation is not None:
-                    colored_translation = "\x1b[96m{}\x1b[m".format(translation)
-                    print("\n", colored_translation)
-                else:
-                    print("\nTranslation not found!")
-                word_on_screen = ""
-                CACHE_PREVIOUS += CACHE_NEXT
-                CACHE_PREVIOUS.append(search)
-                CACHE_NEXT = []
-                is_need_prompt = True
-        elif cmd == UNKNOWN:
-            print("\nNot valid command!")
-            is_need_prompt = True
+    def main_loop(self):
+        self.execute_command("help")
+        try:
+            while True:
+                self.set_prompt_if_need()
+                search, cmd = get_search_word_with_command(self._command.current_search_word)
+                self.execute_command(cmd.lower(), search_word=search)
+        except ExitCommand:
+            print("\nBuy!")
+
+
+    
+def main():
+    translator = Translator()
+    translator.main_loop() 
+
 
 if __name__ == '__main__':
     main()
