3b9d16b332c9b79fced6335fe573e9caa7721a4d
alexey-soldatenko <alexso1datenko86@gmail.com>
2019-06-15 16:12:27 +0300

    Ability to add non exists translations to db

--- custom_std.py
+++ custom_std.py
@@ -1,33 +1,24 @@
 import termios
 import sys
 import tty
+import io
 
 
-UP = b'\x1b[A'
-DOWN = b'\x1b[B'
-BACKSPACE = b'\x7f'
-NEW_LINE = b'\n'
+ESCAPE = '\x1b'
+UP = '[A'
+DOWN = '[B'
+BACKSPACE = '\x7f'
+NEW_LINE = '\n'
 
 
-TRANSLATE = "TRANSLATE"
-PREVIOUS = "PREVIOUS"
-NEXT = "NEXT"
-HELP = "HELP"
-QUIT = "QUIT"
-UNKNOWN = "UNKNOWN"
+stdin_buffer = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8') 
 
-SYSTEM_COMMAND = {
-    "!quit": QUIT,
-    "!help": HELP
-}
+def get_characters_from_stdin(char_count=1):
+    return stdin_buffer.read(char_count)
 
 
-def get_bytes_from_stdin(number_bytes=1):
-    return sys.stdin.buffer.read(number_bytes)
-
-
-def write_byte_to_stdout(ch):
-    sys.stdout.write(ch.decode())
+def write_characters_to_stdout(ch):
+    sys.stdout.write(ch)
     sys.stdout.flush()
 
 
@@ -46,37 +37,34 @@ def set_cbreak(func):
 
 def clear_stdout_previous_characters(num=1):
     for _ in range(num):
-        write_byte_to_stdout(b'\x1b[D\x1b[K')
+        write_characters_to_stdout('\x1b[D\x1b[K')
 
 
 @set_cbreak
-def get_search_word_with_command(buf: str):
-    command = TRANSLATE
+def get_user_input(buf: str) -> tuple:
+    """
+    Return tuple with result user input and command.
+    """
+    command = "ENTER" 
     while True:
-        ch = get_bytes_from_stdin()
+        ch = get_characters_from_stdin()
         if buf and ch == NEW_LINE:
             break
-        # printed characters
-        if 31 < ord(ch) < 127:
-            buf += ch.decode()
-            write_byte_to_stdout(ch)
-        if ch == BACKSPACE and buf:
-            buf = buf[:-1]
-            clear_stdout_previous_characters()
-        # escape symbols
-        if ord(ch) == 27:
-            ch += get_bytes_from_stdin(number_bytes=2)
-            if ch == UP:
-                command = PREVIOUS
+        elif ch == BACKSPACE:
+            if buf:
+                buf = buf[:-1]
+                clear_stdout_previous_characters()
+        elif ch == ESCAPE:
+            esc_cmd = get_characters_from_stdin(char_count=2)
+            if esc_cmd == UP:
+                command = "UP"
                 break
-            if ch == DOWN:
-                command = NEXT
+            if esc_cmd == DOWN:
+                command = "DOWN"
                 break
-    buf = buf.strip()
-    if buf.startswith("!"):
-        if buf in SYSTEM_COMMAND:
-            command = SYSTEM_COMMAND[buf]
         else:
-            command = UNKNOWN
+            buf += ch
+            write_characters_to_stdout(ch)
+    buf = buf.strip()
     return buf, command
 
--- translator.py
+++ translator.py
@@ -2,13 +2,22 @@ import sqlite3
 
 from custom_std import (
     clear_stdout_previous_characters,
-    get_search_word_with_command,
-    HELP,
-    TRANSLATE,
-    PREVIOUS,
-    NEXT,
-    QUIT,
-    UNKNOWN)
+    get_user_input,
+)
+
+ADD = "ADD"
+TRANSLATE = "TRANSLATE"
+PREVIOUS = "PREVIOUS"
+NEXT = "NEXT"
+HELP = "HELP"
+QUIT = "QUIT"
+UNKNOWN = "UNKNOWN"
+
+SYSTEM_COMMAND = {
+    "!add": ADD,
+    "!quit": QUIT,
+    "!help": HELP
+}
 
 
 class ExitCommand(Exception):
@@ -46,6 +55,10 @@ class Command:
         self.current_search_word = ""
         self.need_prompt = True
 
+    def get_system_command(self, phrase):
+        cmd, *remainder = phrase.split()
+        return SYSTEM_COMMAND[cmd], " ".join(remainder)
+
     def execute(self, command: str, **kwargs):
         method_name = "_run_{}_command".format(command)
         try:
@@ -55,11 +68,28 @@ class Command:
         if callable(run_method):
             run_method(**kwargs)
 
-    def _get_translation_from_db(self, word, db_cursor):
-        db_cursor.execute("SELECT rus FROM dict where en='{}'".format(word))
-        r = db_cursor.fetchone()
-        return r[0] if r is not None else None
+    def _get_translation_from_db(self, word):
+        with sqlite3.connect('en-rus.db') as conn:
+            db_cursor = conn.cursor()
+            db_cursor.execute("SELECT rus FROM dict where en='{}'".format(
+                word))
+            r = db_cursor.fetchone()
+            return r[0] if r is not None else None
 
+    def _run_add_command(self, user_input):
+        eng, *translate = user_input.split()
+        rus = " ".join(translate).strip()
+        if self._get_translation_from_db(eng) is None and rus:
+            with sqlite3.connect('en-rus.db') as conn:
+                c = conn.cursor()
+                c.execute("INSERT INTO dict VALUES('{eng}', '{rus}')".format(
+                    **vars()))
+                print("\nWord added succefuly.")
+        elif not rus:
+            print("\nEnter translation please.")
+        else:
+            print("\nTranslation found for this word.")
+        self.need_prompt = True
 
     def _run_help_command(self, **kwargs):
         s = ("\n---------------------------------------------\n"
@@ -73,34 +103,34 @@ class Command:
         self.need_prompt = True
 
 
-    def _run_translate_command(self, search_word):
+    def _run_translate_command(self, user_input):
         with sqlite3.connect('en-rus.db') as conn:
             c = conn.cursor()
 
-            translation = self._get_translation_from_db(search_word, c)
+            translation = self._get_translation_from_db(user_input)
             if translation is not None:
                 colored_translation = "\x1b[96m{}\x1b[m".format(translation)
                 print("\n", colored_translation)
             else:
                 print("\nTranslation not found!")
             self.current_search_word = ""
-            self._history.add(search_word)
+            self._history.add(user_input)
         self.need_prompt = True
 
 
-    def _run_next_command(self, search_word):
+    def _run_next_command(self, user_input):
         if self._history.has_next():
-            next_word = self._history.get_next(search_word)
-            clear_stdout_previous_characters(len(search_word))
+            next_word = self._history.get_next(user_input)
+            clear_stdout_previous_characters(len(user_input))
             self.current_search_word = next_word
             print(next_word, end="", flush=True)
         self.need_prompt = False
 
 
-    def _run_previous_command(self, search_word):
+    def _run_previous_command(self, user_input):
         if self._history.has_previous():
-            previous = self._history.get_previous(search_word)
-            clear_stdout_previous_characters(len(search_word))
+            previous = self._history.get_previous(user_input)
+            clear_stdout_previous_characters(len(user_input))
             self.current_search_word = previous
             print(previous, end="", flush=True)
         self.need_prompt = False
@@ -119,8 +149,18 @@ class Translator:
     def __init__(self):
         self._command = Command()
 
-    def execute_command(self, command: str, **kwargs):
-        self._command.execute(command, **kwargs)
+    def parse_and_execute(self, key_press: str, input_phrase: str):
+        cmd = TRANSLATE
+        if key_press == "ENTER" and input_phrase.startswith("!"):
+            cmd, input_phrase = self._command.get_system_command(input_phrase)
+        elif key_press == "UP":
+            cmd = PREVIOUS
+        elif key_press == "DOWN":
+            cmd = NEXT
+        self._command.execute(cmd.lower(), user_input=input_phrase)
+
+    def execute_command(self, cmd, **kwargs):
+        self._command.execute(cmd, **kwargs)
 
     def set_prompt_if_need(self):
         if self._command.need_prompt:
@@ -131,8 +171,8 @@ class Translator:
         try:
             while True:
                 self.set_prompt_if_need()
-                search, cmd = get_search_word_with_command(self._command.current_search_word)
-                self.execute_command(cmd.lower(), search_word=search)
+                input_phrase, key_press = get_user_input(self._command.current_search_word)
+                self.parse_and_execute(key_press, input_phrase)
         except ExitCommand:
             print("\nBuy!")
 
